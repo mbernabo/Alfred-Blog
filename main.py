3275388551dfeaa3cb68a1d9d1690dbaac7c8386
Martin Bernabo <mbernabo@gmail.com>
2023-05-09 15:24:44 -0300

    Solución de error de deploy, falta de un requisito

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
